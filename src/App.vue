0a148fb0d2c8a1db0127fc3e94dcd4be7b7641e9
honggang.liu <honggang.liu@shopee.com>
2024-01-02 15:52:58 +0800

    组织saferi自动放大

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  </head>
   <div id="app">
     <router-view/>
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 window.onload = function () {
   document.addEventListener('touchstart', function (event) {
     if (event.touches.length > 1) {
-      event.preventDefault()
+      event.preventDefault();
     }
-  })
+  }, {passive: false});
   document.addEventListener('gesturestart', function (event) {
-    event.preventDefault()
-  })
-}
+    event.preventDefault();
+  });
+};
+
 </script>
 
 <style lang="less">
